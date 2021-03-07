1fc26625591e6886dba8105756dd1d6c7df4483d
Leonardo Rodrigues <leonardolp_rj@hotmail.com>
2021-03-07 09:56:56 -0300

    feat(ScriptsDB) Criando primeira base de dados
    - configurando docker-compose para levantar o banco de dados
    
    - criando tabelas e suas relações iniciais
    
    - inserindo primeiros dados arbitrários
    
    tags: #001 #002

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # python-fast-api
+
+#001 Dados
+:ScriptsDB\MySQL
+  :tables
+    * Ordem de criação das primeiras tabelas:
+      - 1º endereços
+      - 2º pessoa_fisica (foreing key em endereços)
+      - 3º lista_dividas ( foreing key em pessoa_fisica)
+  :data_initial
+    * dados simulados para popular as tabelas
+
+#002 Arquitetura
+:docker-compose
+
---
+++ ScriptsDB/MySQL/data_initial/pessoa_fisica.sql
@@ -0,0 +1,8 @@
+INSERT INTO pessoa_fisica (cpf, nome, nascimento,endereco_id)
+VALUES ('12345678925','Moon Cake',STR_TO_DATE('1-01-2012', '%d-%m-%Y'),1);
+
+INSERT INTO pessoa_fisica (cpf, nome, nascimento,endereco_id)
+VALUES ('12345678927','Gerry Bean',STR_TO_DATE('03-05-1994', '%d-%m-%Y'),2);
+
+INSERT INTO pessoa_fisica (cpf, nome, nascimento,endereco_id)
+VALUES ('13345678927','Joao Figueiredo',STR_TO_DATE('31-07-1992', '%d-%m-%Y'),2);
